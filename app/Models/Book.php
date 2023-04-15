39876df492bcd2869716cc1ab9de3d355e44a3e2
jamols09 <jamols09@github.com>
2023-04-15 20:23:10 +0800

    add fillable to book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['description', 'number_pages', 'retail_price', 'reading_difficulty_rating', 'year_published', 'is_best_seller'];
 }
