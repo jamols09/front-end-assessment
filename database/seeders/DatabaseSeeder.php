0bde70a200ba686e5d2f2869dda31c05f3ef5b80
jamols09 <jamols09@github.com>
2023-04-16 11:04:28 +0800

    reintegrate jetstream

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\Author;
-use App\Models\Book;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,15 +16,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        Author::factory(30)->create();
-        Book::factory(100)->create();
-
-        $author = Author::all();
-
-        Book::all()->each(function ($book) use ($author) {
-            $book->authors()->attach(
-                $author->random(rand(1, 5))->pluck('id')->toArray()
-            );
-        });
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
